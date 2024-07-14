224e029a758d39250b80804b81899eebbb386998
Danny Han <codeoftoad@gmail.com>
2024-07-14 04:31:19 -0400

    Separate global definitions

---
+++ src/GLOBALS.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+
+using std::cout, std::endl, std::string, std::vector, std::unordered_map;
+using std::unique_ptr, std::shared_ptr, std::weak_ptr;
+
+#define SCREEN_W 1280
+#define SCREEN_H  720
+
+#define FPS 60
+
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
+// #define
